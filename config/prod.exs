f3e1fb751e1b9b0c2113eab9e5e9aefe21c796ca
laramackey <lara.mackey@focusrite.com>
2021-03-21 15:59:21 +0000

    fix cors stuff for deployed site

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :midichat, MidichatWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "midichat.gigalixirapp.com", port: 443],
+  check_origin: ["//localhost", "//midichat.gigalixirapp.com"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
