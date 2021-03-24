59492fa0d1ac00239d2b5f30cede4b291b062e74
laramackey <lara.mackey@focusrite.com>
2021-03-24 22:26:07 +0000

    try seperate out chat room & piano logic a bit

--- assets/js/chatRoom.js
+++ assets/js/chatRoom.js
@@ -1,14 +1,11 @@
-import * as Tone from 'tone';
 import { userNameGenerator, userColourGenerator } from './userInfoGenerators';
-import { revertKeyColor, updateKeyColor } from './piano';
+import { PianoController } from './piano';
 
 class ChatRoom {
   constructor(socket) {
     this.userColour = userColourGenerator();
     this.socket = socket;
     this.channel = null;
-    this.synth = new Tone.PolySynth({ voice: Tone.Synth }).toMaster();
-    this.precenses = null;
     this.userName = null;
     this.init();
   }
@@ -16,76 +13,31 @@ class ChatRoom {
     document.getElementById('userName').value = userNameGenerator();
     document.getElementById('joinForm').addEventListener('submit', (e) => {
       e.preventDefault();
-      Tone.start();
-      this.userName = document.getElementById('userName').value;
-      this.channel = this.socket.channel('piano:lobby', {
-        userName: this.userName,
+
+      const piano = new PianoController(this.userColour);
+      piano.startTone();
+
+      const userName = document.getElementById('userName').value;
+      this.userName = userName;
+      piano.setUserName(userName);
+
+      const channel = this.socket.channel('piano:lobby', {
+        userName: userName,
         userColour: this.userColour,
       });
-      this.channel.join();
-      this.handleJoined();
-    });
+      this.channel = channel;
+      piano.setChannel(channel);
+      channel.join();
 
-    try {
-      navigator
-        .requestMIDIAccess()
-        .then(
-          (access) => this.onMidiAccessSuccess(access),
-          onMidiAccessFailure
-        );
-    } catch (err) {
-      document.getElementById('midiDevice').innerHTML =
-        'Browser does not support midi, try Chrome';
-      console.log('Oopsy woopsy', err);
-    }
-    function onMidiAccessFailure(error) {
-      console.log('Oopsy woopsy', error.code);
-    }
-    document.documentElement.ondragstart = function () {
-      return false;
-    };
-    let mouseIsDown = false;
-    document.documentElement.addEventListener('mousedown', function () {
-      mouseIsDown = true;
-    });
-    document.documentElement.addEventListener('mouseup', function () {
-      mouseIsDown = false;
-    });
+      this.getPrecenseList();
+      this.listenForBandMates();
+      this.handleChatMessage();
 
-    const keyboard = document.getElementById('keyboard');
-    for (let key of keyboard.children) {
-      key.addEventListener('mouseover', () => {
-        if (mouseIsDown) this.sendNote(key.id);
+      document.getElementById('messageSubmit').disabled = false;
+      document.getElementById('sendMessage').addEventListener('submit', (e) => {
+        e.preventDefault();
+        this.sendMessage();
       });
-      key.addEventListener('mousedown', () => {
-        this.sendNote(key.id);
-      });
-      key.addEventListener('touchstart', () => {
-        this.sendNote(key.id);
-      });
-      key.addEventListener('mouseleave', () => {
-        this.stopNote(key.id);
-      });
-      key.addEventListener('mouseup', () => {
-        this.stopNote(key.id);
-      });
-      key.addEventListener('touchend', () => {
-        this.stopNote(key.id);
-      });
-    }
-  }
-
-  handleJoined() {
-    this.getPrecenseList();
-    this.listenForBandMates();
-    this.listenForNotes();
-    this.handleChatMessage();
-
-    document.getElementById('messageSubmit').disabled = false;
-
-    document.getElementById('sendMessage').addEventListener('submit', (e) => {
-      e.preventDefault();
-      this.sendMessage();
     });
   }
 
@@ -104,10 +56,8 @@ class ChatRoom {
 
   handleChatMessage() {
     this.channel.on('message', (payload) => {
-      console.dir(payload);
-
-      let chatBox = document.querySelector('#chatBox');
-      let msgBlock = document.createElement('p');
+      const chatBox = document.querySelector('#chatBox');
+      const msgBlock = document.createElement('p');
 
       if (payload.body.message) {
         msgBlock.insertAdjacentHTML(
@@ -119,68 +69,10 @@ class ChatRoom {
     });
   }
 
-  handleMidiMessage(message) {
-    const noteOnMidi = 144;
-    const noteOffMidi = 128;
-    const note = Tone.Midi(message.data[1]).toNote();
-
-    if (message.data[0] === noteOffMidi) {
-      this.stopNote(note);
-    } else if (message.data[0] === noteOnMidi) {
-      this.sendNote(note);
-    }
-  }
-
-  onMidiAccessSuccess(access) {
-    const midiAccess = access;
-
-    const inputs = midiAccess.inputs;
-    const inputIterators = inputs.values();
-
-    const firstInput = inputIterators.next().value;
-
-    if (!firstInput) return;
-    document.getElementById('midiDevice').innerHTML = firstInput.name;
-
-    firstInput.onmidimessage = (message) => this.handleMidiMessage(message);
-  }
-
-  sendNote(note) {
-    if (this.channel) {
-      this.channel.push('play', {
-        name: this.userName,
-        body: { userColour: this.userColour, note },
-      });
-    } else {
-      this.synth.triggerAttack(note, undefined, 1);
-      updateKeyColor({ note, userColour: this.userColour });
-    }
-  }
-
-  stopNote(note) {
-    if (this.channel) {
-      this.channel.push('stop', { name: this.userName, body: { note } });
-    } else {
-      this.synth.triggerRelease(note, undefined, 1);
-      revertKeyColor(note);
-    }
-  }
-
-  listenForNotes() {
-    this.channel.on('play', (payload) => {
-      this.synth.triggerAttack([payload.body.note], undefined, 1);
-      updateKeyColor(payload.body);
-    });
-    this.channel.on('stop', (payload) => {
-      this.synth.triggerRelease([payload.body.note], undefined, 1);
-      revertKeyColor(payload.body.note);
-    });
-  }
-
   getPrecenseList() {
     this.channel.on('presence_state', (state) => {
-      let onlineUserBox = document.querySelector('#usersBox');
-      let onlineUsers = document.createElement('p');
+      const onlineUserBox = document.querySelector('#usersBox');
+      const onlineUsers = document.createElement('p');
       const userData = Object.keys(state)
         .map((user) => Object.assign({ user }, state[user].metas[0].user_data))
         .filter((user) => user.hasOwnProperty('userName'));
@@ -197,10 +89,10 @@ class ChatRoom {
 
   listenForBandMates() {
     this.channel.on('presence_diff', (payload) => {
-      let chatBox = document.querySelector('#chatBox');
-      let msgBlock = document.createElement('p');
-      let onlineUserBox = document.querySelector('#usersBox');
-      let onlineUsers = document.createElement('p');
+      const chatBox = document.querySelector('#chatBox');
+      const msgBlock = document.createElement('p');
+      const onlineUserBox = document.querySelector('#usersBox');
+      const onlineUsers = document.createElement('p');
 
       const joiners = Object.keys(payload.joins)
         .map((user) =>
--- assets/js/piano.js
+++ assets/js/piano.js
@@ -1,10 +1,133 @@
-export const revertKeyColor = (note) => {
-  const key = document.getElementById(note);
-  const keyColour = note.includes('#') ? '#222222' : '#CBCBCB';
-  key.style.backgroundColor = keyColour;
-};
+import * as Tone from 'tone';
 
-export const updateKeyColor = (body) => {
-  const key = document.getElementById(body.note);
-  key.style.backgroundColor = body.userColour;
-};
+export class PianoController {
+  constructor(userColour) {
+    this.channel = null;
+    this.userName = null;
+    this.userColour = userColour;
+    this.synth = new Tone.PolySynth({ voice: Tone.Synth }).toMaster();
+    this.init();
+  }
+  init() {
+    this.startTone();
+    try {
+      navigator
+        .requestMIDIAccess()
+        .then((access) => this.onMidiAccessSuccess(access));
+    } catch (err) {
+      this.onMidiAccessFailure(err);
+    }
+
+    document.documentElement.ondragstart = () => {
+      return false;
+    };
+    let mouseIsDown = false;
+    document.documentElement.addEventListener('mousedown', () => {
+      mouseIsDown = true;
+    });
+    document.documentElement.addEventListener('mouseup', () => {
+      mouseIsDown = false;
+    });
+    const keyboard = document.getElementById('keyboard');
+    for (const key of keyboard.children) {
+      key.addEventListener('mouseover', () => {
+        if (mouseIsDown) this.sendNote(key.id);
+      });
+      key.addEventListener('mousedown', () => {
+        this.sendNote(key.id);
+      });
+      key.addEventListener('touchstart', () => {
+        this.sendNote(key.id);
+      });
+      key.addEventListener('mouseleave', () => {
+        this.stopNote(key.id);
+      });
+      key.addEventListener('mouseup', () => {
+        this.stopNote(key.id);
+      });
+      key.addEventListener('touchend', () => {
+        this.stopNote(key.id);
+      });
+    }
+  }
+
+  onMidiAccessSuccess(access) {
+    const midiAccess = access;
+    const inputs = midiAccess.inputs;
+    const inputIterators = inputs.values();
+    const firstInput = inputIterators.next().value;
+    if (firstInput) {
+      document.getElementById('midiDevice').innerHTML = firstInput.name;
+      firstInput.onmidimessage = (message) => this.handleMidiMessage(message);
+    }
+  }
+
+  onMidiAccessFailure(error) {
+    document.getElementById('midiDevice').innerHTML =
+      'Browser does not support midi, try Chrome';
+    console.log('Oopsy woopsy', error.code);
+  }
+
+  handleMidiMessage(message) {
+    const noteOnMidi = 144;
+    const noteOffMidi = 128;
+    const note = Tone.Midi(message.data[1]).toNote();
+    if (message.data[0] === noteOffMidi) {
+      this.stopNote(note);
+    } else if (message.data[0] === noteOnMidi) {
+      this.sendNote(note);
+    }
+  }
+
+  sendNote(note) {
+    if (this.channel) {
+      this.channel.push('play', {
+        name: this.userName,
+        body: { userColour: this.userColour, note },
+      });
+    } else {
+      this.synth.triggerAttack(note, undefined, 1);
+      updateKeyColor({ note, userColour: this.userColour });
+    }
+  }
+
+  stopNote(note) {
+    if (this.channel) {
+      this.channel.push('stop', { name: this.userName, body: { note } });
+    } else {
+      this.synth.triggerRelease(note, undefined, 1);
+      revertKeyColor(note);
+    }
+  }
+
+  revertKeyColor(note) {
+    const key = document.getElementById(note);
+    const keyColour = note.includes('#') ? '#222222' : '#CBCBCB';
+    key.style.backgroundColor = keyColour;
+  }
+
+  updateKeyColor(body) {
+    const key = document.getElementById(body.note);
+    key.style.backgroundColor = body.userColour;
+  }
+
+  setUserName(userName) {
+    this.userName = userName;
+  }
+
+  setChannel(channel) {
+    this.channel = channel;
+    this.channel.on('play', (payload) => {
+      this.synth.triggerAttack([payload.body.note], undefined, 1);
+      updateKeyColor(payload.body);
+    });
+    this.channel.on('stop', (payload) => {
+      this.synth.triggerRelease([payload.body.note], undefined, 1);
+      revertKeyColor(payload.body.note);
+    });
+  }
+
+  startTone() {
+    Tone.start();
+  }
+}
