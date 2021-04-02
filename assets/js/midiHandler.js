6f87855ede18a6706b59e3a5be76b7f948cbb69e
laramackey <lara.mackey@focusrite.com>
2021-04-02 21:14:21 +0100

    Add ability to choose midi device, & detect connect/ disconnct

--- assets/js/midiHandler.js
+++ assets/js/midiHandler.js
@@ -3,6 +3,7 @@ import * as Tone from 'tone';
 export class MidiHandler {
   constructor(piano) {
     this.piano = piano;
+    this.midiAccess = null;
     this.init();
   }
   init() {
@@ -13,17 +14,62 @@ export class MidiHandler {
     } catch (err) {
       this.onMidiAccessFailure(err);
     }
+    document
+      .getElementById('selectMidiDevice')
+      .addEventListener('change', () => {
+        const selectedDevice = document.getElementById('selectMidiDevice')
+          .value;
+        const inputs = this.midiAccess.inputs.values();
+        for (
+          let input = inputs.next();
+          input && !input.done;
+          input = inputs.next()
+        )
+          if (input.value.name === selectedDevice) {
+            input.value.onmidimessage = (message) =>
+              this.handleMidiMessage(message);
+          } else {
+            input.value.onmidimessage = '';
+          }
+      });
   }
 
   onMidiAccessSuccess(access) {
-    const midiAccess = access;
-    const inputs = midiAccess.inputs;
-    const inputIterators = inputs.values();
-    const firstInput = inputIterators.next().value;
-    if (firstInput) {
-      document.getElementById('midiDevice').innerHTML = firstInput.name;
-      firstInput.onmidimessage = (message) => this.handleMidiMessage(message);
+    this.midiAccess = access;
+    if (access.inputs && access.inputs.size > 0) {
+      const inputs = access.inputs.values();
+      for (
+        let input = inputs.next();
+        input && !input.done;
+        input = inputs.next()
+      ) {
+        const opt = document.createElement('option');
+        opt.text = input.value.name;
+        document.getElementById('selectMidiDevice').add(opt);
+      }
     }
+
+    access.onstatechange = (e) => {
+      const midiDropdown = document.getElementById('selectMidiDevice');
+      const currentOptions = Array.from(midiDropdown.options).map(
+        (option) => option.value
+      );
+      if (
+        e.port.state === 'connected' &&
+        !currentOptions.includes(e.port.name)
+      ) {
+        const opt = document.createElement('option');
+        opt.text = e.port.name;
+        midiDropdown.add(opt);
+      }
+      if (
+        e.port.state === 'disconnected' &&
+        currentOptions.includes(e.port.name)
+      ) {
+        const disconnectedDeviceIndex = currentOptions.indexOf(e.port.name);
+        midiDropdown.remove(disconnectedDeviceIndex);
+      }
+    };
   }
 
   onMidiAccessFailure(error) {
